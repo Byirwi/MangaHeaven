4c5cc3dedcb27e54e8855a8558b955b209ed2a0d
Byirwi <depaiva.louka@gmail.com>
2025-04-03 18:37:08 +0200

    Update image paths in Accueil.php to use relative paths

--- HTML_PHP/Accueil.php
+++ HTML_PHP/Accueil.php
@@ -26,23 +26,23 @@
             <div class="manga-container">
                 <!-- Liste des mangas populaires -->
                 <div class="manga-item">
-                    <img src="images/Naruto.jpg" alt="Naruto">
+                    <img src="../images/Naruto.jpg" alt="Naruto">
                     <h3>Naruto</h3>
                 </div>
                 <div class="manga-item">
-                    <img src="images/one_piece.jpg" alt="One Piece">
+                    <img src="../images/one_piece.jpg" alt="One Piece">
                     <h3>One Piece</h3>
                 </div>
                 <div class="manga-item">
-                    <img src="images/attack_on_titan.jpg" alt="Attack on Titan">
+                    <img src="../images/attack_on_titan.jpg" alt="Attack on Titan">
                     <h3>Attack on Titan</h3>
                 </div>
                 <div class="manga-item">
-                    <img src="images/my_hero_academia.jpg" alt="My Hero Academia">
+                    <img src="../images/my_hero_academia.jpg" alt="My Hero Academia">
                     <h3>My Hero Academia</h3>
                 </div>
                 <div class="manga-item">
-                    <img src="images/arifureta.jpg" alt="Arifureta">
+                    <img src="../images/arifureta.jpg" alt="Arifureta">
                     <h3>Arifureta</h3>
                 </div>
             </div>
