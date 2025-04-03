6b4e4eec66f8b219444f06e4846ec57792464d90
Byirwi <depaiva.louka@gmail.com>
2025-04-03 22:55:49 +0200

    Add navigation links for improved accessibility on Home, Account, and Login pages

--- HTML_PHP/Page_Compte/Compte.php
+++ HTML_PHP/Page_Compte/Compte.php
@@ -194,6 +194,7 @@ $conn->close();
             <ul>
                 <li><a href="../Page_Accueil/Accueil.php">Accueil</a></li>
                 <li><a href="../Page_Home/Home.php">home</a></li>
+                <li><a href="../Page_Compte/Compte.php">Mon Compte</a></li>
                 <li><a href="../logout.php">Déconnexion</a></li>
             </ul>
         </nav>
